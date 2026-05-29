cbc72cfacd8b9824337dca0c09fe6fbd92a201de
KishanPatel <patelkishan3101@gmail.com>
2026-05-29 23:54:22 +0530

    Dashboard Completed

--- client/src/assets/assets.jsx
+++ client/src/assets/assets.jsx
@@ -8,27 +8,20 @@ import img_2 from "./img-2.jpg";
 import img_3 from "./img-3.jpg";
 import img_4 from "./img-4.jpg";
 
-const LinkedInIcon = (props: React.SVGProps<SVGSVGElement>) => (
+const LinkedInIcon = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em" {...props}>
         <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
     </svg>
 );
 
-export interface Platform {
-    id: string;
-    name: string;
-    icon: React.ElementType;
-    description: string;
-}
-
-export const PLATFORMS: Platform[] = [
+export const PLATFORMS = [
     { id: "twitter", name: "Twitter / X", icon: SiX, description: "Post tweets, threads, and media" },
     { id: "linkedin", name: "LinkedIn", icon: LinkedInIcon, description: "Publish to your profile & company pages" },
     { id: "facebook", name: "Facebook", icon: SiFacebook, description: "Manage your pages and profile" },
     { id: "instagram", name: "Instagram", icon: SiInstagram, description: "Share photos, reels and stories" },
 ];
 
-export const dummyPostsData: any = [
+export const dummyPostsData = [
     {
         _id: "69fd98f4a59dd584d3587a1e",
         user: "69fd96aea59dd584d3587a1d",
